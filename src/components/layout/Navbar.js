ddfcc9b88de7866cddf1b5bd8abf5a772c5a754c
Anurag <anurag.sach28@gmail.com>
2019-06-26 15:12:48 +0530

    react portion complete

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOnLinks from './SignedOutLinks';
+
+
+
+const Navbar = () => {
+    return (
+        <nav className={"nav-wrapper grey darken-3"}>
+            <div className={"container"}>
+                <Link to="/" className="brand-logo" > MarioPlan </Link>
+                <SignedInLinks/>
+                <SignedOnLinks/>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
